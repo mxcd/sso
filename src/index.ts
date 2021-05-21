10ae83025d8c5dfc9d87328fb5c29b2fa5b15450
Max Partenfelder <max.partenfelder@wilde-it.com>
2021-05-21 19:59:08 +0200

    added oidc config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,40 @@
-import express from 'express';
 const { ApolloServer } =  require('apollo-server-express');
 import { createServer } from 'http';
 import compression from 'compression';
-import cors from 'cors';
-
-import schema from './schema/schemas';
+// import cors from 'cors';
+const helmet = require('helmet');
+const { Provider } =  require('oidc-provider');
+const providerConfig = require('../provider-config')
 
 import {prisma, log, env, contextBuilder} from "./util";
+
+let express = require("express");
+if(!env.production) {
+    require("https-localhost");
+}
+
+const {API_PORT = 3000, ISSUER_URL = "https://localhost:3000"} = env
+
+import schema from './schema/schemas';
 
 async function main() {
     log.info(`Generating express server`)
     const app = express();
+    app.use(helmet());
+    app.set('view engine', 'pug')
+    app.set('views', './views')
+
+    const oidc = new Provider(ISSUER_URL, {
+        ...providerConfig,
+        async findAccount(ctx, id) {
+            return {
+                accountId: id,
+                async claims(use, scope) { return { sub: id }; },
+            };
+        }
+    });
+
+    app.use(oidc.callback());
 
     log.info(`Generating Apollo server`)
     const index = new ApolloServer({
@@ -19,7 +43,7 @@ async function main() {
     });
 
     log.info(`Applying middlewares`)
-    app.use('*', cors());
+    // app.use('*', cors());
     app.use(compression());
     index.applyMiddleware({ app, path: '/graphql' });
 
@@ -27,7 +51,7 @@ async function main() {
     const httpServer = createServer(app);
 
     log.info(`Starting server`)
-    httpServer.listen({ port: env.API_PORT }, (): void => console.log(`GraphQL is now running on port ${env.API_PORT}`));
+    httpServer.listen({ port: API_PORT }, (): void => console.log(`GraphQL is now running on port ${API_PORT}`));
 }
 
 main()
---
+++ src/schema/types.ts
@@ -0,0 +1,193 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  DateTime: any;
+};
+
+export type DeleteUserGroupInput = {
+  id: Scalars['Int'];
+};
+
+export type EditUserGroupInput = {
+  id: Scalars['Int'];
+  name?: Maybe<Scalars['String']>;
+  displayName?: Maybe<Scalars['String']>;
+  description?: Maybe<Scalars['String']>;
+};
+
+export type CreateUserGroupInput = {
+  name: Scalars['String'];
+  displayName: Scalars['String'];
+  description: Scalars['String'];
+};
+
+export type DeleteUserInput = {
+  id: Scalars['Int'];
+};
+
+export type CreateUserInput = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type EditUserInput = {
+  id: Scalars['Int'];
+  email?: Maybe<Scalars['String']>;
+  groups?: Maybe<Array<Scalars['String']>>;
+  validated?: Maybe<Scalars['Boolean']>;
+  locked?: Maybe<Scalars['Boolean']>;
+};
+
+export type SignInInput = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type SignUpUserInput = {
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  me: User;
+  users?: Maybe<UsersPayload>;
+  user: User;
+  userGroups?: Maybe<UserGroupsPayload>;
+  userGroup: UserGroup;
+};
+
+
+export type QueryUsersArgs = {
+  search?: Maybe<Scalars['String']>;
+  limit?: Maybe<Scalars['Int']>;
+  after?: Maybe<Scalars['Int']>;
+};
+
+
+export type QueryUserArgs = {
+  id: Scalars['Int'];
+};
+
+
+export type QueryUserGroupsArgs = {
+  search?: Maybe<Scalars['String']>;
+  limit?: Maybe<Scalars['Int']>;
+  after?: Maybe<Scalars['Int']>;
+};
+
+
+export type QueryUserGroupArgs = {
+  id: Scalars['Int'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  signUp: SignUpPayload;
+  signIn: SignInPayload;
+  signOut: Scalars['Boolean'];
+  createUser: User;
+  editUser: User;
+  deleteUser: Scalars['Boolean'];
+  createUserGroup: UserGroup;
+  editUserGroup: UserGroup;
+  deleteUserGroup: Scalars['Boolean'];
+};
+
+
+export type MutationSignUpArgs = {
+  input?: Maybe<SignUpUserInput>;
+};
+
+
+export type MutationSignInArgs = {
+  input?: Maybe<SignInInput>;
+};
+
+
+export type MutationCreateUserArgs = {
+  input?: Maybe<CreateUserInput>;
+};
+
+
+export type MutationEditUserArgs = {
+  input?: Maybe<EditUserInput>;
+};
+
+
+export type MutationDeleteUserArgs = {
+  input?: Maybe<DeleteUserInput>;
+};
+
+
+export type MutationCreateUserGroupArgs = {
+  input?: Maybe<CreateUserGroupInput>;
+};
+
+
+export type MutationEditUserGroupArgs = {
+  input?: Maybe<EditUserGroupInput>;
+};
+
+
+export type MutationDeleteUserGroupArgs = {
+  input?: Maybe<DeleteUserGroupInput>;
+};
+
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['Int'];
+  email: Scalars['String'];
+  validated: Scalars['Boolean'];
+  locked: Scalars['Boolean'];
+  groups: Array<UserGroup>;
+  createdBy: Scalars['String'];
+  createdDate: Scalars['DateTime'];
+  modifiedBy: Scalars['String'];
+  modifiedDate: Scalars['DateTime'];
+};
+
+export type UserGroup = {
+  __typename?: 'UserGroup';
+  id: Scalars['Int'];
+  name: Scalars['String'];
+  displayName: Scalars['String'];
+  description: Scalars['String'];
+  users: Array<User>;
+  createdBy: Scalars['String'];
+  createdDate: Scalars['DateTime'];
+  modifiedBy: Scalars['String'];
+  modifiedDate: Scalars['DateTime'];
+};
+
+export type SignUpPayload = {
+  __typename?: 'SignUpPayload';
+  user: User;
+  accessToken: Scalars['String'];
+};
+
+export type SignInPayload = {
+  __typename?: 'SignInPayload';
+  accessToken: Scalars['String'];
+};
+
+export type UserGroupsPayload = {
+  __typename?: 'UserGroupsPayload';
+  userGroups: Array<UserGroup>;
+  total: Scalars['Int'];
+};
+
+export type UsersPayload = {
+  __typename?: 'UsersPayload';
+  users: Array<User>;
+  total: Scalars['Int'];
+};
